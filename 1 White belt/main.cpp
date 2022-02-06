8e2dba42f14b8a6a5d465f73b0020a909f87d111
peter <gorkovenko.petr@gmail.com>
2022-02-06 17:39:37 +0300

    Add Date and Database class interfaces

---
+++ 1 White belt/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+
+using namespace std;
+
+class Date {
+public:
+    int GetYear() const;
+    int GetMonth() const;
+    int GetDay() const;
+};
+
+bool operator<(const Date& lhs, const Date& rhs);
+
+class Database {
+public:
+    void AddEvent(const Date& date, const string& event);
+    bool DeleteEvent(const Date& date, const string& event);
+    int  DeleteDate(const Date& date);
+
+    string Find(const Date& date) const;
+
+    void Print() const;
+};
+
+int main() {
+    Database db;
+
+    string command;
+    while (getline(cin, command)) {
+
+
+    }
+
+    return 0;
+}
