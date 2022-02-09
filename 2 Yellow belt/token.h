a43773423e6c21032f7a95d90321d24a4d4061a6
peter <gorkovenko.petr@gmail.com>
2022-02-09 19:20:49 +0300

    Add necessary classes for condition parser to token.h file

--- 2 Yellow belt/token.h
+++ 2 Yellow belt/token.h
@@ -2,13 +2,28 @@
 
 #include <sstream>
 #include <vector>
+
 using namespace std;
+
+enum class LogicalOperation {
+    Or,
+    And,
+};
+
+enum class Comparison {
+    Less,
+    LessOrEqual,
+    Greater,
+    GreaterOrEqual,
+    Equal,
+    NotEqual,
+};
 
 enum class TokenType {
     DATE,
     EVENT,
     COLUMN,
-    LOGICAL_OP,r
+    LOGICAL_OP,
     COMPARE_OP,
     PAREN_LEFT,
     PAREN_RIGHT,
@@ -19,4 +34,4 @@ struct Token {
     const TokenType type;
 };
 
-vector<Token> Tokenize(istream& cl);
+vector<Token> Tokenize(istream &cl);
